77ef2fe930600fb73ae822c451d4438b236b03eb
danilosenati <danilosena.ti@gmail.com>
2025-04-01 12:26:06 -0300

    update codes

--- aula 1/javaScript.js
+++ aula 1/javaScript.js
@@ -1,2 +1,9 @@
-valor = prompt("Digite um valor: ")
-alert(valor);
+valorWons = prompt("Digite um valor em wons: ");
+// alert(valorWons);
+
+cotacaoWons = 0.0040;
+
+valorConvertido = valorWons*cotacaoWons;
+
+// Para converter em Real.
+alert(cotacaoWons);
