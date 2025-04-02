3c06310ea3c40fbe3024f8ce9090fb6565d8955e
danilosenati <danilosena.ti@gmail.com>
2025-04-01 22:21:10 -0300

    update 1.1

--- aula 1/javaScript.js
+++ aula 1/javaScript.js
@@ -12,9 +12,20 @@ alert("R$: " + valorConvertido);
 // alert(valorConvertido);
 
 function conversor(){
+    cotacaoReal = 0.0040;
+    cotacaoDolar = 0.00068;
+    cotacaoEuro = 0.00063;
+
     valorWons = prompt("Digite um valor em wons: ");
-    cotacaoWons = 0.0040;
     
-    valorConvertido = valorWons*cotacaoWons;
-    alert("R$: " + valorConvertido);
+    resposta = prompt("Digite: 1 para REAL | 2 para DOLAR | 3 para EURO: ");
+    if(resposta == 1){
+        valorConvertido = valorWons * cotacaoReal;
+    }else if(resposta == 2){
+        valorConvertido = valorWons * cotacaoDolar;
+    }else{
+        valorConvertido = valorWons * cotacaoEuro;
+    }
+ 
+    alert("Valor convertido: " + valorConvertido);
 }
