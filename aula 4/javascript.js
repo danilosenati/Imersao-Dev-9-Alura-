5c40e92d8e95ff47e32516fd87b821fc7a619d6c
danilosenati <danilosena.ti@gmail.com>
2025-04-04 00:16:23 -0300

    aula 4.1

---
+++ aula 4/javascript.js
@@ -0,0 +1,7 @@
+let rodada = 1;
+resultado = 0; // SE FOR 1 , PERDEU. SE FOR 0, GANHOU.
+
+for(rodada = 1; rodada <= 3; rodada++){
+    escolhaJogador = prompt("Nível " + rodada + ": Escolha um vidro (1, 2, 3)");
+    pisoQuebrado = Math.floor(Math.random() * 3 +1);
+ }
